00b7192db26d6ba6e758429fd7083a834d05d0ea
Bartłomiej Stępień <bartek.bartlomiej.96@gmail.com>
2021-03-22 23:51:39 +0100

    Introduce QuantumExperiment and GateTest; refactor CCCX gate's test

--- tests/test_cccx.py
+++ tests/test_cccx.py
@@ -1,39 +1,53 @@
 import unittest
-from itertools import chain
+from typing import List, Tuple, Dict, Callable
 
-from qiskit import Aer, QuantumCircuit
+from qiskit.circuit import Gate
 
 from gates.cccx import triple_controlled_not
-from tests.wip import test_gate
+from tests.wip import QuantumExperiment, GateTest
 
 
 class CCCXTestCase(unittest.TestCase):
     def test_triple_controlled_not(self):
+        test = CCCXTest(self)
 
-        def append_gate(qc: QuantumCircuit):
-            qc.append(
-                triple_controlled_not(),
-                list(chain.from_iterable(qc.qregs))
+        params = [{'ctrl': ctrl, 'x': x, 'g': g} for g in range(2) for x in range(2) for ctrl in reversed(range(8))]
+        test.run_subtests(params)
+
+
+class CCCXTest(GateTest):
+    def __init__(self, test_case: unittest.TestCase):
+        super().__init__(TripleControlledNotExperiment(), test_case)
+        self._ctrl: int = 0
+
+    def _update_params(self, initial_values: Dict[str, int]) -> None:
+        self._ctrl = initial_values['ctrl']
+
+    def _get_name(self, initial_values: Dict[str, int]) -> str:
+        return f'CCCX({self._ctrl}, {initial_values["x"]})_4_qubits'
+
+    @property
+    def _custom_asserts(self) -> Dict[str, Tuple[Callable[[int], int], Callable[[int, int, int], str]]]:
+        return {
+            'x': (
+                lambda x: (x + 1) % 2 if self._ctrl == 0b111 else x,
+                lambda x, y, v: f'Wrong value after experiment (expected CCCX({self._ctrl}, {x}) = {y}, got {v})'
             )
+        }
 
-        ctrl = 7
 
-        def test_triple_controlled_negation(x: int, value: int) -> bool:
-            expected_value = (x + 1) % 2 if ctrl == 7 else x
-            print(expected_value, value, expected_value == value)
-            return expected_value == value
+class TripleControlledNotExperiment(QuantumExperiment):
+    @property
+    def _qregs_spec(self) -> List[Tuple[str, int]]:
+        return [
+            ('ctrl', 3),
+            ('x', 1),
+            ('g', 1),
+        ]
 
-        backend = Aer.get_backend('qasm_simulator')
-
-        test_gate(
-            backend,
-            regs_data=[('ctrl', 3), ('x', 1), ('g', 1)],
-            initial_values={'ctrl': ctrl, 'x': 1},
-            append_gate=append_gate,
-            asserts={'x': test_triple_controlled_negation}
-        )
-
-        self.assertEqual(True, False)
+    @property
+    def _gate(self) -> Gate:
+        return triple_controlled_not()
 
 
 if __name__ == '__main__':
--- tests/wip.py
+++ tests/wip.py
@@ -1,33 +1,51 @@
-from typing import Callable, Dict, List, Tuple
+import unittest
+from abc import ABC, abstractmethod
+from itertools import chain
+from typing import Dict, List, Tuple, Callable
 
-from qiskit import QuantumRegister, QuantumCircuit, ClassicalRegister, execute
+from qiskit import QuantumRegister, QuantumCircuit, ClassicalRegister, execute, Aer
+from qiskit.circuit import Gate
 from qiskit.providers import Backend
 from qiskit.result import Result
 
 from utils import as_bits_reversed
 
 
-def test_gate(backend: Backend,
-              regs_data: List[Tuple[str, int]],
-              initial_values: Dict[str, int],
-              asserts: Dict[str, Callable[[int, int], bool]],
-              append_gate: Callable[[QuantumCircuit], None]):
+class QuantumExperiment(ABC):
+    def __init__(self, backend: Backend = Aer.get_backend('qasm_simulator')):
+        self._backend = backend
 
-    circuit = _create_circuit(regs_data)
-    _initialize(circuit, initial_values)
-    append_gate(circuit)
-    _measure(circuit)
+    def run(self, initial_values: Dict[str, int], name: str) -> Result:
+        circuit = self._construct_circuit(initial_values, name)
+        job = execute(circuit, self._backend, shots=1)
+        return job.result()
 
-    job = execute(circuit, backend, shots=1)
-    result = job.result()
+    def _construct_circuit(self, initial_values: Dict[str, int], name: str) -> QuantumCircuit:
+        circuit = _create_circuit(self._qregs_spec, name)
+        _initialize(circuit, initial_values)
+        _append_gate(circuit, self._gate)
+        _measure(circuit)
 
-    values = _parse_result(result, circuit)
-    _assert(circuit, values, initial_values, asserts)
+        return circuit
+
+    @property
+    @abstractmethod
+    def _qregs_spec(self) -> List[Tuple[str, int]]:
+        pass
+
+    @property
+    @abstractmethod
+    def _gate(self) -> Gate:
+        pass
+
+    @property
+    def qreg_names(self) -> List[str]:
+        return [name for (name, _) in self._qregs_spec]
 
 
-def _create_circuit(regs_data: List[Tuple[str, int]]) -> QuantumCircuit:
+def _create_circuit(regs_data: List[Tuple[str, int]], name: str) -> QuantumCircuit:
     qregs = [QuantumRegister(size, name=name) for (name, size) in regs_data]
-    return QuantumCircuit(*qregs)
+    return QuantumCircuit(*qregs, name=name)
 
 
 def _initialize(circuit: QuantumCircuit, values: Dict[str, int]) -> None:
@@ -42,6 +60,11 @@ def _initialize(circuit: QuantumCircuit, values: Dict[str, int]) -> None:
     circuit.barrier(*circuit.qregs)
 
 
+def _append_gate(circuit: QuantumCircuit, gate: Gate) -> None:
+    qubits = list(chain.from_iterable(circuit.qregs))
+    circuit.append(gate, qubits)
+
+
 def _measure(circuit: QuantumCircuit) -> None:
     qregs = circuit.qregs
     cregs = [ClassicalRegister(qreg.size, name=f'{qreg.name}Value') for qreg in qregs]
@@ -53,26 +76,52 @@ def _measure(circuit: QuantumCircuit) -> None:
         circuit.measure(qreg[:], creg[:])
 
 
-def _parse_result(result: Result, circuit: QuantumCircuit) -> List[int]:
-    keys = list(result.get_counts(circuit).keys())
-    assert len(keys) == 1
+class GateTest(ABC):
+    def __init__(self, experiment: QuantumExperiment, test_case: unittest.TestCase):
+        self._experiment = experiment
+        self.test_case = test_case
 
-    values_as_str = keys[0].split(' ')
-    values = [int(value, 2) for value in values_as_str]
-    return values[::-1]
+    def run_subtests(self, params: List[Dict[str, int]]):
+        for initial_values in params:
+            with self.test_case.subTest(initial_values):
+                self.run(initial_values)
 
+    def run(self, initial_values: Dict[str, int]):
+        self._update_params(initial_values)
 
-def _assert(circuit: QuantumCircuit,
-            values: List[int],
-            initial_values: Dict[str, int],
-            asserts: Dict[str, Callable[[int, int], bool]]) -> None:
+        name = self._get_name(initial_values)
+        result = self._experiment.run(initial_values, name)
 
-    for qreg, value in zip(circuit.qregs, values):
-        assert_ = asserts.get(qreg.name, _default_assert)
-        initial_value = initial_values.get(qreg.name, 0)
+        asserts = self._custom_asserts
+        for name, value in zip(self._experiment.qreg_names, self._parse_result(result, name)):
+            initial_value = initial_values.get(name, 0)
 
-        print(initial_value, value, assert_(initial_value, value))
+            if name in asserts:
+                (compute, msg) = asserts[name]
+                expected_value = compute(initial_value)
+                self.test_case.assertEqual(value, expected_value,
+                                           msg(initial_value, expected_value, value))
+            else:
+                self.test_case.assertEqual(value, initial_value,
+                                           f'Wrong value in register "{name}" (expected {initial_value}, got {value})')
 
+    def _parse_result(self, result: Result, name: str) -> List[int]:
+        output = list(result.get_counts(name).keys())
+        self.test_case.assertEqual(len(output), 1)
 
-def _default_assert(initial_value: int, final_value: int) -> bool:
-    return initial_value == final_value
+        values_as_str = output[0].split(' ')
+        values = [int(value, 2) for value in values_as_str]
+        return values[::-1]
+
+    @abstractmethod
+    def _update_params(self, initial_values: Dict[str, int]) -> None:
+        pass
+
+    @abstractmethod
+    def _get_name(self, initial_values: Dict[str, int]) -> str:
+        pass
+
+    @property
+    @abstractmethod
+    def _custom_asserts(self) -> Dict[str, Tuple[Callable[[int], int], Callable[[int, int, int], str]]]:
+        pass
