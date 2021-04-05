24802eec2715047346d35c91fc6c7cd3e782e65e
Bartłomiej Stępień <bartek.bartlomiej.96@gmail.com>
2021-04-06 01:29:02 +0200

    Optimize ancillary register in Carry gate (from article)

--- gates/carry.py
+++ gates/carry.py
@@ -8,15 +8,21 @@ from utils.typing_ import QRegsSpec
 
 def carry(constant, n) -> Gate:
     circuit = create_circuit(carry_regs(n), f'Carry_({constant})')
-    x_qreg, g_qreg, c_qreg = circuit.qregs
 
-    body = _carry_body(constant, n, x_qreg, g_qreg)
-    qubits = x_qreg[:] + g_qreg[:]
+    if n == 1:
+        x_qreg, c_qreg = circuit.qregs
+        if constant == 1:
+            circuit.cx(x_qreg[0], c_qreg[0])
+    else:
+        x_qreg, g_qreg, c_qreg = circuit.qregs
 
-    circuit.cx(g_qreg[n - 1], c_qreg)
-    circuit.append(body, qubits)
-    circuit.cx(g_qreg[n - 1], c_qreg)
-    circuit.append(body.inverse(), qubits)
+        body = _carry_body(constant, n, x_qreg, g_qreg)
+        qubits = x_qreg[:] + g_qreg[:]
+
+        circuit.cx(g_qreg[n-2], c_qreg)
+        circuit.append(body, qubits)
+        circuit.cx(g_qreg[n-2], c_qreg)
+        circuit.append(body.inverse(), qubits)
 
     return circuit.to_gate()
 
@@ -25,17 +31,21 @@ def _carry_body(constant, n, x_qreg, g_qreg):
     circuit = QuantumCircuit(x_qreg, g_qreg, name=f'Carry_({constant})_body')
     constant_bits = as_bits_reversed(constant, n)
 
-    for i in reversed(range(1, n)):
+    for i in reversed(range(2, n)):
         if constant_bits[i] == '1':
-            circuit.cx(x_qreg[i], g_qreg[i])
+            circuit.cx(x_qreg[i], g_qreg[i-1])
             circuit.x(x_qreg[i])
-        circuit.ccx(g_qreg[i - 1], x_qreg[i], g_qreg[i])
+        circuit.ccx(g_qreg[i-2], x_qreg[i], g_qreg[i-1])
+
+    if constant_bits[1] == '1':
+        circuit.cx(x_qreg[1], g_qreg[0])
+        circuit.x(x_qreg[1])
 
     if constant_bits[0] == '1':
-        circuit.cx(x_qreg[0], g_qreg[0])
+        circuit.ccx(x_qreg[0], x_qreg[1], g_qreg[0])
 
-    for i in range(1, n):
-        circuit.ccx(g_qreg[i - 1], x_qreg[i], g_qreg[i])
+    for i in range(2, n):
+        circuit.ccx(g_qreg[i-2], x_qreg[i], g_qreg[i-1])
 
     return circuit.to_gate()
 
@@ -46,8 +56,14 @@ def carry_regs(n: int) -> QRegsSpec:
     |g> - dirty ancillary register; initial state must be restored
     |carry> - output register - information about carry
     """
-    return [
-        ('x', n),
-        ('g', n),
-        ('carry', 1)
-    ]
+    if n == 1:
+        return [
+            ('x', n),
+            ('carry', 1)
+        ]
+    else:
+        return [
+            ('x', n),
+            ('g', n-1),
+            ('carry', 1)
+        ]
--- tests/test_carry.py
+++ tests/test_carry.py
@@ -5,8 +5,16 @@ from tests.gate_tests.carry import Test
 
 class CarryTestCase(unittest.TestCase):
     def test_too_much(self):
-        for i in range(1, 5):
+        for i in range(2, 5):
             self._test_for_n_qubits(i)
+
+    def test_for_1_qubit(self):
+        n = 1
+        N = 2 ** n
+        G = 2 ** n
+        for constant in range(N):
+            with self.subTest(constant=constant):
+                self._test(constant, n, N, G)
 
     def _test_for_n_qubits(self, n: int):
         N = 2 ** n
