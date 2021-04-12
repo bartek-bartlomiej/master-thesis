a586e0278d98d94c5eec5bd264515673e15b7523
Bartłomiej Stępień <bartek.bartlomiej.96@gmail.com>
2021-04-12 10:23:02 +0200

    Add carry gate tests

--- tests/test_carry.py
+++ tests/test_carry.py
@@ -4,29 +4,67 @@ from tests.gate_tests.carry import Test
 
 
 class CarryTestCase(unittest.TestCase):
-    def test_too_much(self):
-        for i in range(2, 5):
-            self._test_for_n_qubits(i)
+    def test_passing_wrong_value_for_constant(self):
+        n = 4
+        x = 8
+        g = 3
+
+        constant = -1
+        with self.assertRaises(ValueError):
+            test = Test(constant, n, self)
+            test.run({'x': x, 'g': g})
+
+        constant = 16
+        with self.assertRaises(OverflowError):
+            test = Test(constant, n, self)
+            test.run({'x': x, 'g': g})
 
     def test_for_1_qubit(self):
         n = 1
-        N = 2 ** n
-        G = 2 ** n
-        for constant in range(N):
-            with self.subTest(constant=constant):
-                self._test(constant, n, N, G)
+        values = [0, 1]
 
-    def _test_for_n_qubits(self, n: int):
-        N = 2 ** n
-        G = 2 ** (n - 1)
-        for constant in range(N):
+        for constant in values:
             with self.subTest(constant=constant):
-                self._test(constant, n, N, G)
+                params = [{'x': x} for x in values]
 
-    def _test(self, constant, n, N, G):
+                test = Test(constant, n, self)
+                test.run_subtests(params)
+
+    def test_for_2_qubits(self):
+        self._test_for_n_qubits(2, [0, 1, 2, 3], 1)
+
+    def test_dirty_register_for_2_qubits(self):
+        self._test_dirty_register_for_n_qubits(2, 2, 1, [0, 1])
+
+    def test_for_3_qubits(self):
+        self._test_for_n_qubits(3, [0, 1, 3, 5, 6, 7], 2)
+
+    def test_dirty_register_for_3_qubits(self):
+        self._test_dirty_register_for_n_qubits(3, 5, 4, [0, 1, 2, 3])
+
+    def test_for_4_qubits(self):
+        self._test_for_n_qubits(4, [0, 1, 5, 10, 12, 14, 15], 5)
+
+    def test_dirty_register_for_4_qubits(self):
+        self._test_dirty_register_for_n_qubits(4, 12, 7, [0, 1, 5, 6, 7])
+
+    def test_for_5_qubits(self):
+        self._test_for_n_qubits(5, [0, 1, 8, 17, 25, 30, 31], 10)
+
+    def test_dirty_register_for_5_qubits(self):
+        self._test_dirty_register_for_n_qubits(5, 25, 8, [0, 1, 5, 10, 14, 15])
+
+    def _test_for_n_qubits(self, n, values, g):
+        for constant in values:
+            with self.subTest(constant=constant):
+                params = [{'x': x, 'g': g} for x in values]
+
+                test = Test(constant, n, self)
+                test.run_subtests(params)
+
+    def _test_dirty_register_for_n_qubits(self, n, constant, x, g_values):
+        params = [{'x': x, 'g': g} for g in g_values]
         test = Test(constant, n, self)
-        params = [{'x': x, 'g': g}
-                  for g in range(G) for x in range(N)]
         test.run_subtests(params)
 
 
--- utils/bits.py
+++ utils/bits.py
@@ -1,4 +1,8 @@
 def as_bits(x: int, n: int) -> str:
+    if x < 0:
+        raise ValueError(f'int ({x}) should be >= 0')
+    if x.bit_length() > n:
+        raise OverflowError(f'int ({x}) too big to represent with {n} bits')
     return f'{x:0{n}b}'
 
 
