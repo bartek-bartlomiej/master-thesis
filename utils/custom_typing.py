561fb73632ed3de8bbb4568a16c48c8f6faf8faa
Bartłomiej Stępień <bartek.bartlomiej.96@gmail.com>
2023-02-18 16:45:28 +0100

    Delete gates tests and draft notebooks from main branch

--- utils/custom_typing.py
+++ utils/custom_typing.py
@@ -1,13 +1,8 @@
-from typing import Tuple, List, Callable, Dict
+from typing import Dict
 
 Size = int
 Name = str
 
 QRegsSpec = Dict[Name, Size]
 
-Value = int
-Computation = Callable[[Value], Value]
-ComputationsMap = Dict[Name, Computation]
-ValuesMap = Dict[Name, Value]
-
-del Tuple, List, Callable, Dict
+del Dict
